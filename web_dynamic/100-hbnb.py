32b076ea5b24cf5f71fb94d69176e829a341724e
fatimaelasri01 <fatimaelassri041@gmail.com>
2024-05-06 00:07:46 +0100

    up

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -18,12 +18,13 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/100-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
+    cache_id = uuid.uuid4()
 
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
@@ -34,7 +35,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('100-hbnb.html',
                            cache_id=uuid.uuid4(),
                            states=st_ct,
                            amenities=amenities,
